1b2af34fbfce0c87480baa40f76017cdd791003e
MD. MAZHARUL ISLAM <mislamdev@mitech.stream>
2026-06-10 00:12:44 +0600

    chore(live-tv): comment out Obiram TV and RoarZone Live channel entries

--- app/services/live-tv/page.tsx
+++ app/services/live-tv/page.tsx
@@ -5,8 +5,8 @@ import { getAssetPath } from '@/lib/utils';
 
 export default function LiveTVPage() {
   const liveTVServices = [
-    { name: 'Obiram TV', url: 'https://obiramtvlive.pages.dev/', image: getAssetPath('/assets/img/allservices/obiram-tv.jpg') },
-    { name: 'RoarZone Live', url: 'https://tv.roarzone.net/', image: getAssetPath('/assets/img/allservices/roarzone-tv.jpg') },
+    // { name: 'Obiram TV', url: 'https://obiramtvlive.pages.dev/', image: getAssetPath('/assets/img/allservices/obiram-tv.jpg') },
+    // { name: 'RoarZone Live', url: 'https://tv.roarzone.net/', image: getAssetPath('/assets/img/allservices/roarzone-tv.jpg') },
     { name: 'NeT-PoinT Live', url: 'http://97.74.103.44/jwtv1/index.php', image: getAssetPath('/assets/img/allservices/sports.jpg') },
     { name: 'Live Tv Sports', url: 'http://172.17.50.112', image: getAssetPath('/assets/img/allservices/net-point-bd.jpg') },
     { name: 'LIVE Mazic TV', url: 'http://moviemazic.xyz/live-tv.html', image: getAssetPath('/assets/img/allservices/bdip-live.jpg') },
